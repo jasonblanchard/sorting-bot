d6ab3f66d80bec8624c469c03c442c6eb1f7741b
Jason Blanchard <jas.blanchard@gmail.com>
2016-11-01 22:41:56 -0400

    Removed token. Oops!

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 import Botkit from 'botkit';
 
+require('dotenv').config();
+
 const controller = Botkit.slackbot({
   debug: false,
 });
 
 controller.spawn({
-  token: 'xoxb-99340059573-TOiPf7k3uUB2us2NG6VoRvST'
+  token: process.env.BOT_TOKEN,
 }).startRTM();
 
-controller.hears('hello', ['direct_message', 'mention'], (bot, message) => {
-  console.log(message);
-  bot.reply(message, 'Got it!');
-})
+controller.hears('test', ['direct_message', 'mention'], (bot, message) => {
+  bot.reply(message, 'Working!');
+});
 
 const houses = {
-  'slytherin': {
+  slytherin: {
     name: 'slytherin',
     users: [],
-    points: 0
+    points: 0,
   },
-  'gryffindor': {
+  gryffindor: {
     name: 'gryffindor',
     users: [],
-    points: 0
-  }
+    points: 0,
+  },
 };
 
-controller.hears('sort', ['direct_message', 'mention'], (bot, message) => {
-  console.log(message);
+controller.hears('sort (.+) (.+)', ['direct_message', 'mention'], (bot, message) => {
   bot.reply(message, 'Sorted');
+  const { match } = message;
+  const user = match[1];
+  const house = match[2];
+
+  if (!houses[house].users.includes(user)) houses[house].users.push(user);
+
+  bot.reply(message, `Sorted ${user} in house ${house}`);
+});
+
+controller.hears('list houses', ['direct_message', 'mention'], (bot, message) => {
+  bot.reply(message, JSON.stringify(houses));
 });
